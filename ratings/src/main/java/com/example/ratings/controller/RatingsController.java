4028838e7eb2754f0f06f1954f77d3b36d2aa29b
nitin <nitin@pop-os.localdomain>
2019-08-21 21:26:18 +0100

    Fully functional.

--- ratings/src/main/java/com/example/ratings/controller/RatingsController.java
+++ ratings/src/main/java/com/example/ratings/controller/RatingsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -26,43 +27,60 @@ public class RatingsController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/user/{userId}")
     public RatingDTO getByUserId(@PathVariable int userId) {
-        RestTemplate restTemplate = new RestTemplate();
-        RatingDTO ratingDTO = new RatingDTO();
-        User user = restTemplate.getForObject("http://localhost:8082/user/"+userId, User.class);
-        ratingDTO.setName(user.getName());
 
         List<Rating> ratings = ratingRepository.findByUid(userId);
+
+        RestTemplate restTemplate = new RestTemplate();
+        RatingDTO ratingDTO = new RatingDTO();
+        ArrayList<Pair> list = new ArrayList<>();
+
+        User user = restTemplate.getForObject("http://localhost:8082/user/"+userId, User.class);
         System.out.println("TinTin : ratings = "+ratings);
+
         ratings.stream()
                 .map((Function<Rating, Pair>) rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8083/movie"+rating.getMid(), Movie.class);
+                    Movie movie = restTemplate.getForObject("http://localhost:8083/movie/"+rating.getMid(), Movie.class);
                     System.out.println("TinTin : movie = "+movie);
                     return Pair.of(movie.getName(), rating.getRating());
                 })
-                .forEach(pair -> ratingDTO.getList().add(pair));
+                .forEach(list::add);
+
+        if(user != null) {
+            ratingDTO.setName(user.getName());
+            ratingDTO.setList(list);
+        }
+
         System.out.println("TinTin : ratingDTO = "+ratingDTO);
         return ratingDTO;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/movie/{movieId}")
     public RatingDTO getByMovieId(@PathVariable int movieId) {
-        RestTemplate restTemplate = new RestTemplate();
-        RatingDTO result = new RatingDTO();
-        Movie movie = restTemplate.getForObject("http://localhost:8083/movie"+movieId, Movie.class);
-        result.setName(movie.getName());
 
         List<Rating> ratings = ratingRepository.findByMid(movieId);
+
+        RestTemplate restTemplate = new RestTemplate();
+        RatingDTO ratingDTO = new RatingDTO();
+        ArrayList<Pair> list = new ArrayList<>();
+
+        Movie movie = restTemplate.getForObject("http://localhost:8083/movie/"+movieId, Movie.class);
 
         ratings.stream()
                 .map((Function<Rating, Pair>) rating -> {
                     User user = restTemplate.getForObject("http://localhost:8082/user/"+rating.getUid(), User.class);
                     return Pair.of(user.getName(), rating.getRating());
                 })
-                .forEach(pair -> result.getList().add(pair));
-        return result;
+                .forEach(list::add);
+
+        if(movie != null) {
+            ratingDTO.setName(movie.getName());
+            ratingDTO.setList(list);
+        }
+
+        return ratingDTO;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/userId/{userId}")
+    /*@RequestMapping(method = RequestMethod.GET, value = "/userId/{userId}")
     public List<Rating> getByUId(@PathVariable int userId) {
         return ratingRepository.findByUid(userId);
     }
@@ -75,5 +93,5 @@ public class RatingsController {
     @RequestMapping(method = RequestMethod.GET, value = "/rating/{rating}")
     public List<Rating> getByRating(@PathVariable int rating) {
         return ratingRepository.findByRating(rating);
-    }
+    }*/
 }
