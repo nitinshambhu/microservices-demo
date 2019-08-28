8d754f445a999002d2180b61abcee949313ea952
nitin <nitin@pop-os.localdomain>
2019-08-28 21:28:59 +0100

    Added a method findById() to UserRepository.

--- users/src/main/java/com/example/users/repo/UserRepository.java
+++ users/src/main/java/com/example/users/repo/UserRepository.java
@@ -5,8 +5,11 @@ import com.example.users.model.User;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends Repository<User, Integer> {
 
     List<User> findAll();
+
+    Optional<User> findById(Integer id);
 }
