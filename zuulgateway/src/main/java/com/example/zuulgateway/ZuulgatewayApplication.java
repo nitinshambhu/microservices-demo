161522dd339f2ce1ada4dd4824693862ff78f7ce
Nitin Shambhu <nitin.X.shambhu@aib.ie>
2020-02-27 16:43:27 +0000

    Added Zuul gateway instance

---
+++ zuulgateway/src/main/java/com/example/zuulgateway/ZuulgatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulgatewayApplication.class, args);
+	}
+
+}
